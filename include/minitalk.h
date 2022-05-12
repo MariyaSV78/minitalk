0b51bf5c6c257c8c910a913db461b94557112fd6
Mariya SOSNOVA <msosnova@e3r3p8.clusters.42paris.fr>
2022-05-12 13:52:53 +0200

    1

---
+++ include/minitalk.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msosnova <msosnova@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/07 11:36:21 by msosnova          #+#    #+#             */
+/*   Updated: 2021/11/08 21:07:33 by msosnova         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stddef.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+void	ft_putnbr(int n);
+int		ft_atoi(const char *nptr);
+void	ft_send_signal(pid_t pid, int signum);
+
+#endif
---
+++ server.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msosnova <msosnova@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/07 10:57:28 by msosnova          #+#    #+#             */
+/*   Updated: 2021/11/08 20:57:57 by msosnova         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minitalk.h>
+
+void	get_byte(int signal, siginfo_t *info, void *context)
+{
+	static unsigned char	byte = 0;
+	static unsigned char	tmp_byte = 1;
+
+	(void) context;
+	if (signal == SIGUSR1)
+		byte |= tmp_byte;
+	tmp_byte <<= 1;
+	if (tmp_byte == 0)
+	{	
+		ft_putchar(byte);
+		byte = 0;
+		tmp_byte = 1;
+	}
+	ft_send_signal(info->si_pid, SIGUSR1);
+}
+
+int	main(void)
+{
+	pid_t				pid;
+	struct sigaction	sig;
+
+	pid = getpid();
+	ft_putstr("server pid: ");
+	ft_putnbr(pid);
+	write (1, "\n", 1);
+	sig.sa_flags = SA_SIGINFO;
+	sig.sa_sigaction = get_byte;
+	sig.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &sig, NULL);
+	sigaction(SIGUSR2, &sig, NULL);
+	while (1)
+		pause ();
+	return (0);
+}
